3c6a4696f6907a52f3305187531ec91be5386d4d
Nicoly Avelino <nicoly@om.interno>
2023-09-30 23:18:32 -0300

    :construction:Primeira seção da Home

--- NoPonto-FrontEnd/src/components/Navbar/index.jsx
+++ NoPonto-FrontEnd/src/components/Navbar/index.jsx
@@ -14,18 +14,18 @@ export default function Navbar() {
     return (
         <div className='shadow-md w-full fixed top-0 left-0'>
             <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-                {/* logo section */}
+                {/* Logo */}
                 <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
                     <CursorArrowRaysIcon className='w-7 h-7 text-colorDarkGreen' />
                     <span>NoPonto</span>
                 </div>
-                {/* Menu icon */}
-                <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
+                {/* Icon - Mobile */}
+                <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7 z-50'>
                     {
                         open ? <XMarkIcon /> : <Bars3BottomRightIcon />
                     }
                 </div>
-                {/* linke items */}
+                {/* Link dos items menu */}
                 <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
                     {Links.map((link, index) => (
                         <li key={index} className='md:ml-8 md:my-0 my-7 font-semibold'>
@@ -36,7 +36,6 @@ export default function Navbar() {
                     ))}
                     <button className='btn bg-colorDarkGreen text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static'>Sign up</button>
                 </ul>
-                {/* button */}
             </div>
         </div>
 
--- NoPonto-FrontEnd/src/pages/Home/index.jsx
+++ NoPonto-FrontEnd/src/pages/Home/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import './style.css'
+import arvores from '../../assets/arvores.jpg'
 
 function Home() {
-  const pageStyle = {
-    backgroundColor: 'blue',
-    minHeight: '100vh', 
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-  };
-
   return (
-    <div style={pageStyle}>
-      <h1>Bem-vindo à Página Inicial</h1>
+    <div
+      className='w-full h-screen'
+      style={{
+        backgroundImage: `url(${arvores})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className='top-0 w-full h-full flex flex-col justify-center text-white'>
+        <div className='md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
+          <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>
+            Encontre os pontos de coleta mais próximos de você.
+          </h1>
+          <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>
+            Descubra locais onde você pode fazer o descarte consciente de seus resíduos. Faça a diferença para o meio ambiente e para o futuro do nosso planeta.
+          </p>
+          <button className='bg-colorAccent w-[200px] hover:bg-colorDarkGreen rounded-md font-medium my-6 mx-auto py-4 text-white'>Buscar ponto de coleta</button>
+        </div>
+      </div>
     </div>
+
   );
 }
 
