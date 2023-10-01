a42106a1a9ae2cd62ce3efe5e43499fd895b7e82
Nicoly Avelino <nicoly@om.interno>
2023-09-30 21:27:58 -0300

    :construction:Feito o Footer

--- NoPonto-FrontEnd/src/components/Footer/index.jsx
+++ NoPonto-FrontEnd/src/components/Footer/index.jsx
@@ -1,7 +1,52 @@
+import { Typography } from "@material-tailwind/react";
+import { CursorArrowRaysIcon } from '@heroicons/react/24/solid'
+export default function Footer() {
+  return (
+    <footer className="w-full bg-colorMidGreen p-8">
+      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-colorMidGreen text-center md:justify-between">
+        {/* LOGO */}
+        <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
+          <CursorArrowRaysIcon className='w-7 h-7 text-white' />
+          <span className="text-white">NoPonto</span>
+        </div>
 
-  export default function Footer() {
-    return (
-      <div>footer</div>
-    );
-  }
-  
+        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal text-white transition-colors hover:text-colorGreen focus:text-colorGreen"
+            >
+              Sobre
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal text-white transition-colors hover:text-colorGreen focus:text-colorGreen"
+            >
+              Licença
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal text-white transition-colors hover:text-colorGreen focus:text-colorGreen"
+            >
+              Contato
+            </Typography>
+          </li>
+        </ul>
+      </div>
+      <hr className="my-8 border-blue-gray-50" />
+      <Typography color="blue-gray" className="text-center text-white font-normal">
+        &copy; 2023
+      </Typography>
+    </footer>
+  );
+}
